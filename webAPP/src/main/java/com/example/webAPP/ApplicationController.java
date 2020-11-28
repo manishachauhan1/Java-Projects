3d43d9cb82188bbc880f4eca4f450de96f5e5d34
Manisha Chauhan <manisha@xecurify.com>
2020-11-29 00:36:05 +0530

    Spring boot web app created

---
+++ webAPP/src/main/java/com/example/webAPP/ApplicationController.java
@@ -0,0 +1,12 @@
+package com.example.webAPP;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ApplicationController {
+    @GetMapping("/index")
+    public String goHome(){
+        return "index";
+    }
+}
